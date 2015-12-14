86d3121861e25464d3179d71dc3ebaa847d7d2da
130101N <nadeeshani.13@cse.mrt.ac.lk>
2015-12-14 12:19:39 +0530

    interface was developed

---
+++ WindowsGame3/WindowsGame3/Tile.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsGame3
+{
+    static class Tile
+    {
+        static public Texture2D TileSetTexture;
+
+        static public Rectangle GetSourceRectangle(int tileIndex)
+        {
+            return new Rectangle(tileIndex * 50, 0, 50, 50);
+        }
+
+
+    }
+}
